c44e6049f764c253a28db991b7521e46f0ddb63f
firstname middlename lastname <vahid8r@gmail.com>
2026-01-23 15:29:26 +0330

    oi mate whenever i manage to push this shit out y'all won't BELIEVE how much shit has been changed!

--- aperture/obj/Release/net9.0-windows/aperture.AssemblyInfo.cs
+++ aperture/obj/Release/net9.0-windows/aperture.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aperture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ee08b16455e2d0565bd1a4084de1eee1f644bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb5e1e87792cfc658b7c0c45924a7930430b348b")]
 [assembly: System.Reflection.AssemblyProductAttribute("aperture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aperture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
