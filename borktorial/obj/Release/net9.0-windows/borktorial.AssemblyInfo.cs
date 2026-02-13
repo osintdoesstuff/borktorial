5e731673154dfd1aad9c18bdd0db5abe04705de3
firstname middlename lastname <vahid8r@gmail.com>
2026-02-13 04:00:47 +0330

    ggggggggggggggggggggggggggggggg

--- borktorial/obj/Release/net9.0-windows/borktorial.AssemblyInfo.cs
+++ borktorial/obj/Release/net9.0-windows/borktorial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("borktorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aff39951355be8579c8ac504e8e8774ca30ba3a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5208312048223c3277f629e72a31e09dcfdd08af")]
 [assembly: System.Reflection.AssemblyProductAttribute("borktorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("borktorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
