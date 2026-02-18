f1193d0a392d064850b5b996d80c9f52d70ed8df
firstname middlename lastname <vahid8r@gmail.com>
2026-02-18 10:51:07 +0330

    fixed clusterfuck

--- aperture/obj/Release/net9.0-windows/aperture.AssemblyInfo.cs
+++ aperture/obj/Release/net9.0-windows/aperture.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aperture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57fc2a327fe7cc071046b49969a98e12c14a8e6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f6bab5f44c383a4df2cf821273e8d0d04e3b8e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("aperture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aperture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
